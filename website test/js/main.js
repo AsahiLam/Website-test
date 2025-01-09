a2dd955d044d5dfd7627ce6e3646cd96f8c07b8e
AsahiLam <haison2025389@gmail.com>
2025-01-09 14:23:34 +0700

    update for checking

--- website test/js/main.js
+++ website test/js/main.js
@@ -1,14 +1,31 @@
-window.addEventListener('load', function() {
-    var blockElement = document.querySelector('.block');
-    
-    function updateBlockClass() {
-      if (window.innerWidth < 769) {
-        blockElement.classList.add('block--vertical');
-      } else {
-        blockElement.classList.remove('block--vertical');
-      }
+window.addEventListener("load", function () {
+  // Select all blocks
+  var blockElements = document.querySelectorAll(".block");
+
+  function handleMouseOver(event) {
+    if (window.innerWidth < 769) {
+      event.target.classList.add("block--vertical");
     }
-      updateBlockClass();
-  
-    window.addEventListener('resize', updateBlockClass);
+  }
+
+  function handleMouseOut(event) {
+    if (window.innerWidth < 769) {
+      event.target.classList.remove("block--vertical");
+    }
+  }
+
+  // Attach event listeners to each block
+  blockElements.forEach(function (block) {
+    block.addEventListener("mouseover", handleMouseOver);
+    block.addEventListener("mouseout", handleMouseOut);
   });
+
+  // Update class on resize
+  window.addEventListener("resize", function () {
+    blockElements.forEach(function (block) {
+      if (window.innerWidth >= 769) {
+        block.classList.remove("block--vertical");
+      }
+    });
+  });
+});
