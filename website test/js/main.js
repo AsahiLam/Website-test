2928996781d4306f2f5fdeb1085590bb7183a4c9
AsahiLam <haison2025389@gmail.com>
2025-01-10 10:42:24 +0700

    10/1

--- website test/js/main.js
+++ website test/js/main.js
@@ -1,40 +1,41 @@
 window.addEventListener("load", function () {
-  var blockElement = document.querySelector(".block");
-
-  function updateBlockClass() {
-    if (window.innerWidth < 769) {
-      blockElement.classList.add("block--vertical");
-    } else {
-      blockElement.classList.remove("block--vertical");
-    }
-  }
-  updateBlockClass();
-
-  window.addEventListener("resize", updateBlockClass);
-});
-
-window.onload = function () {
   const blockItems = document.querySelectorAll(".block__item");
 
   blockItems.forEach((item) => {
-    item.addEventListener("mouseenter", () => {
-      const readingTime = item.querySelector(
-        ".block__image-area__meta-reading-time"
-      );
-      if (readingTime) {
+    const readingTime = item.querySelector(
+      ".block__image-area__meta-reading-time"
+    );
+
+    if (readingTime) {
+      item.addEventListener("mouseenter", () => {
         readingTime.style.opacity = "1";
         readingTime.style.pointerEvents = "auto";
-      }
-    });
+      });
 
-    item.addEventListener("mouseleave", () => {
-      const readingTime = item.querySelector(
-        ".block__image-area__meta-reading-time"
-      );
-      if (readingTime) {
+      item.addEventListener("mouseleave", () => {
         readingTime.style.opacity = "0";
         readingTime.style.pointerEvents = "none";
-      }
-    });
+      });
+    }
   });
-};
+
+  const block = document.querySelectorAll(".block");
+
+  blockItems.forEach((item) => {
+    const readingTime = item.querySelector(
+      ".block__image-area__meta-reading-time"
+    );
+
+    if (readingTime) {
+      item.addEventListener("mouseenter", () => {
+        readingTime.style.opacity = "1";
+        readingTime.style.pointerEvents = "auto";
+      });
+
+      item.addEventListener("mouseleave", () => {
+        readingTime.style.opacity = "0";
+        readingTime.style.pointerEvents = "none";
+      });
+    }
+  });
+});
